81551b31eef0fc35358d10ab417b929ec55d60da
Ronak1215 <139199657+Ronak1215@users.noreply.github.com>
2024-06-20 21:17:03 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <img src="src\assets\img\pngwing.com.png" alt="" srcset="" className='h-11 w-15'/>
+  )
+}
+
+export default Logo
---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./container/Container"
+import Logo from "./Logo";
+import LogoutBtn from './Header/LogoutBtn'
+import RTE from "./RTE";
+import SignUp from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
+import Button from "./Button"
+import Select from "./Select"
+
+
+export{
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  SignUp,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Button, 
+  Select,
+}
